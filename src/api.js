136018a7d6577f7af31a09b0d6346dd38b622693
Mayran Aragão <mayran.aragao@armazemparaiba.com.br>
2021-11-01 09:05:34 -0300

    Alteraçao de url base

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const baseURL = 'http://172.16.45.121:3003'
+const baseURL = 'https://pontomobile.armazemparaiba.com.br/'
+// const baseURL = 'https://api.github.com/'
 
 function timeout(ms, promise) {
     return new Promise(function (resolve, reject) {
@@ -32,7 +33,7 @@ const request = async (method, endpoint, params, token = null) => {
     if (token) {
         headers.Authorization = `Bearer ${token}`
     }
-    let json = timeout(5000, fetch(fullUrl, { method, headers, body })).then(async (response) => {
+    let json = timeout(7000, fetch(fullUrl, { method, headers, body })).then(async (response) => {
         let json = await response.json()
         return json
     }).catch(async (error) => {
@@ -46,6 +47,10 @@ const request = async (method, endpoint, params, token = null) => {
 export default {
     getToken: async () => {
         return await AsyncStorage.getItem('token')
+    },
+    getOtilio: async () => {
+        let json = await request('get', 'users/paulootilio')
+        return json
     },
     getUser: async () => {
         return await AsyncStorage.getItem('user')
